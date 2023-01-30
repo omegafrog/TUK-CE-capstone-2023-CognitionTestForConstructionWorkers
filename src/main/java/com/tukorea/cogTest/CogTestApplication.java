7b1ba9e0c9a2a5ffc36ffc46e72c6f03d8e5f232
omegafrog <jiwoo22110103@gmail.com>
2023-01-30 15:36:00 +0900

    Initial commit

---
+++ src/main/java/com/tukorea/cogTest/CogTestApplication.java
@@ -0,0 +1,13 @@
+package com.tukorea.cogTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CogTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CogTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tukorea/cogTest/CogTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tukorea.cogTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CogTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
