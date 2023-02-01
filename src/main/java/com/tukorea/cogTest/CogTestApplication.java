66ce0e9dc94831101db51db831e4ba530c061f8a
omegafrog <jiwoo22110103@gmail.com>
2023-02-01 22:36:48 +0900

    feat : Doesn't make jar -plain file anymore and Make boot.pid that notify pid

--- src/main/java/com/tukorea/cogTest/CogTestApplication.java
+++ src/main/java/com/tukorea/cogTest/CogTestApplication.java
@@ -2,12 +2,15 @@ package com.tukorea.cogTest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class CogTestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CogTestApplication.class, args);
+		SpringApplication application = new SpringApplication(CogTestApplication.class);
+		application.addListeners(new ApplicationPidFileWriter());
+		application.run(CogTestApplication.class, args);
 	}
 
 }
