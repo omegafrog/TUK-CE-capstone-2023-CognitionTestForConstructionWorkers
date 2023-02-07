e826f95f6d43611794a8d8005773fe6eb18bdb7a
omegafrog <jiwoo22110103@gmail.com>
2023-02-07 11:35:25 +0900

    feat : Add spring security config for h2-console

--- src/main/java/com/tukorea/cogTest/security/WebSecurityConfig.java
+++ src/main/java/com/tukorea/cogTest/security/WebSecurityConfig.java
@@ -1,6 +1,21 @@
 package com.tukorea.cogTest.security;
 
 // TODO : spring security 가 h2console을 막아서 접근을 못함. webconfigurer만들어야함.
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    SecurityFilterChain web(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                (authorize) -> authorize.anyRequest().authenticated());
+        return http.build();
+    }
 }
