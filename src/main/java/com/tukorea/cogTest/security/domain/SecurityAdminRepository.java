4ef28e540afcecadedcaa29998157fd0cb5054a7
omegafrog <jiwoo22110103@gmail.com>
2023-03-14 22:15:53 +0900

    feat: SecurityAdmin 구현 삭제

--- src/main/java/com/tukorea/cogTest/security/domain/SecurityAdminRepository.java
+++
@@ -1,6 +0,0 @@
-package com.tukorea.cogTest.security.domain;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SecurityAdminRepository extends JpaRepository<SecurityAdmin, Long> {
-    SecurityAdmin findByUsername(String username);
-}
