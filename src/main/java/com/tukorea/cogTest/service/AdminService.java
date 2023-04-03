5481faae34ba5e96c61e3743ce053fbef348b4ad
omegafrog <jiwoo22110103@gmail.com>
2023-04-03 19:02:15 +0900

    feat : adminservice의 loadUserByUsername 수정
    loadByUsername에서 UserDetail을 생성할 때 Authorities를 넣었는데, Role을 넣도록 변경

--- src/main/java/com/tukorea/cogTest/service/AdminService.java
+++ src/main/java/com/tukorea/cogTest/service/AdminService.java
@@ -49,7 +49,7 @@ public class AdminService implements UserDetailsService {
             Admin foundedAdmin = adminRepository.findByUsername(username);
             return User.withUsername(username)
                     .password(foundedAdmin.getPassword())
-                    .authorities("ROLE_"+foundedAdmin.getRole().value)
+                    .roles(foundedAdmin.getRole().value)
                     .build();
         }catch (IllegalArgumentException e){
             log.error("msg={}", e.getMessage());
