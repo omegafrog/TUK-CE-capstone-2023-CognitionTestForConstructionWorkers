1573da402275f1986573ecd90f6adb37a8173420
omegafrog <jiwoo22110103@gmail.com>
2023-04-26 19:30:15 +0900

    hotfix : add workers의 권한 변경
    workers의 권한이 admin으로 생성되어서 USER 권한을 가지도록 변경

--- backend/src/main/java/com/tukorea/cogTest/service/AdminServiceImpl.java
+++ backend/src/main/java/com/tukorea/cogTest/service/AdminServiceImpl.java
@@ -109,7 +109,7 @@ public class AdminServiceImpl implements AdminService {
                 .age(subjectForm.getAge())
                 .username(subjectForm.getUsername())
                 .password(encoder.encode(subjectForm.getPassword()))
-                .role(Role.ADMIN)
+                .role(Role.USER)
                 .risk(Risk.NORMAL)
                 .field(foundedField)
                 .build();
@@ -125,7 +125,7 @@ public class AdminServiceImpl implements AdminService {
                 .name(adminForm.getName())
                 .username(adminForm.getUsername())
                 .password(encoder.encode(adminForm.getPassword()))
-                .role(Role.ADMIN)
+                .role(Role.USER)
                 .field(selectedField)
                 .position(adminForm.getPosition())
                 .build();
