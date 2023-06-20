403bca784937d30b11437e4b92a524e59942c1f9
omegafrog <jiwoo22110103@gmail.com>
2023-06-20 15:25:58 +0900

    jwt filter의 예외 사항 체크

--- backend/src/main/java/com/tukorea/cogTest/security/filter/JwtFilter.java
+++ backend/src/main/java/com/tukorea/cogTest/security/filter/JwtFilter.java
@@ -11,7 +11,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -22,54 +22,67 @@ import java.io.IOException;
 import java.util.Date;
 
 @RequiredArgsConstructor
+@Slf4j
 public class JwtFilter extends OncePerRequestFilter {
 
-    @Value("${jwt.secret}")
-    private String secret;
+    private final String secret;
     private final AdminService adminService;
     private final SubjectService subjectService;
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        log.info("jwt filter 시작");
         // 로그인 페이지는 필요 없음.
         String requestURI = request.getRequestURI();
-        String mainResource = requestURI.split("/")[0];
-        if(requestURI.contains("login") || requestURI.contains("register")){
+        String mainResource = requestURI.split("/")[1];
+        if (requestURI.contains("login") || requestURI.contains("register")) {
+            log.info("로그인,회원가입 페이지 진입");
             filterChain.doFilter(request, response);
             return;
         }
         String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
         if (authorizationHeader == null) {
             // 토큰 헤더 없음.
+            log.info("토큰 헤더 없음");
+            filterChain.doFilter(request, response);
+            return;
         }
         String[] tokenArray = authorizationHeader.split(" ");
         if (!tokenArray[0].equals("Bearer")) {
             // 인증 타입 다름
+            log.info("인증 타입 다름");
+            filterChain.doFilter(request, response);
+            return;
         }
         if (tokenArray.length != 2) {
             // 토큰 온전하지 않음
+            log.info("토큰 온전하지 않음");
+            filterChain.doFilter(request, response);
+            return;
         }
 
         // 맞는지 확인
         String jwtToken = tokenArray[1];
         Claims claims = TokenUtil.extractClaim(secret, jwtToken);
         Date expiration = claims.getExpiration();
-        String id = (String) claims.get("id");
-        // 만료됨
-        if (expiration.after(new Date(System.currentTimeMillis()))) {
+        String id = (String) claims.get("userId");
 
+        // 만료됨
+        if (expiration.before(new Date(System.currentTimeMillis()))) {
+            log.info("토큰 만료됨");
+            filterChain.doFilter(request, response);
+            return;
         }
         // 로그아웃 체크
 
-        if(mainResource.equals("admin")){
+        if (mainResource.equals("admin")) {
             AdminDTO byId = adminService.findById(Long.valueOf(id));
             UserDetails user = adminService.loadUserByUsername(byId.getUsername());
 
             UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
                     user.getUsername(), user.getPassword(), user.getAuthorities()
             );
-            token.setAuthenticated(true);
             SecurityContextHolder.getContext().setAuthentication(token);
-        }else if(mainResource.equals("subject")){
+        } else if (mainResource.equals("subject")) {
             SubjectDTO byId = subjectService.findSubject(Long.valueOf(id));
             UserDetails user = subjectService.loadUserByUsername(byId.getUsername());
 
@@ -79,5 +92,6 @@ public class JwtFilter extends OncePerRequestFilter {
             token.setAuthenticated(true);
             SecurityContextHolder.getContext().setAuthentication(token);
         }
+        filterChain.doFilter(request,response);
     }
 }
