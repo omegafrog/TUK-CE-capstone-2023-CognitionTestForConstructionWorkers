9008534bcc81f6014d54edcabafeb73ea41f50b0
js-kim99 <128694778+js-kim99@users.noreply.github.com>
2023-09-03 17:28:43 +0900

    Revert "minor change at DMT, main menu"
    This reverts commit ad640950c04d72355c7719dd34a4f17fae53360f.

--- Unity_project/Assets/DMT_code/DecisionMakingTest_tuto.cs
+++ Unity_project/Assets/DMT_code/DecisionMakingTest_tuto.cs
@@ -7,9 +7,6 @@ using UnityEngine.SceneManagement;
 
 public class DecisionMakingTest_tuto : MonoBehaviour
 {
-
-    public static string PracticeAvgResult;
-
     private GameObject PracticeGamestartPannel;
     private Button PracticeGamestartbutton;
 
@@ -19,20 +16,6 @@ public class DecisionMakingTest_tuto : MonoBehaviour
     private TextMesh PracticeQuestionDisplay; // 사용자가 입력한 숫자를 유니티 화면에 표시해줄 텍스트를 저장할 TextMesh 컴포넌트   
 
     public static bool isDMTstarted;
-
-    public Text DMT_Go_missed;
-    public Text DMT_NoGo_clicked;
-    public Text DMT_AvgResult;
-    public Text DMT_ProcessCounting;
-
-    public static int Go_clicked, Go_missed, NoGo_clicked; //버튼 클릭 판별
-    public static float Sum_GoClickTime, Avg_GoClickTime;//계산 결과 담을 변수
-
-    private int ProcessCounting;
-    private float startTime;      // 시간 측정 시작 시간
-    private float buttonTime;     // 버튼이 눌린 시간
-    private float elapsedTime;    // 경과 시간
-    public static bool buttonPressed;   // 버튼이 눌렸는지 여부
 
     // Start is called before the first frame update
     void Start()
@@ -46,25 +29,17 @@ public class DecisionMakingTest_tuto : MonoBehaviour
         PracticeQuestionDisplay.text = "+";
 
 
-        PracticeGoList = "ㄹ";
+        PracticeGoList = "ㄱㄴㄷㄹㅁㅂㅅㅇㅈㅊㅋㅌㅍㅎ";
         PracticeGoNoGoQuestion = "ㄹㅓ4";
 
         PracticeGamestartbutton.onClick.AddListener(PracticeButtonClick);//게임시작
 
-        Go_clicked = 0;
-        Go_missed = 0;
-        NoGo_clicked = 0;
-        Sum_GoClickTime = 0;
-        Avg_GoClickTime = 0;
-        ProcessCounting = 0;
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        ButtonClick();
-        ShowScore();
     }
 
 
@@ -76,26 +51,9 @@ public class DecisionMakingTest_tuto : MonoBehaviour
         StartCoroutine(PracticeGoNoGoGame());
 
     }
-    public void ButtonClick()//버튼 클릭할 수 있게 하는 함수
-    {
-        if (Input.GetMouseButtonDown(0)) // 마우스 클릭 시
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
 
-            if (Physics.Raycast(ray, out hit))
-            {
-                if (hit.collider.CompareTag("DMT_PracticeInputButton")) //이 이름의 태그를 가진 버튼 오브젝트를 눌렀을 때
-                {
-                    buttonPressed = true;
-                    buttonTime = Time.time;
 
-                }
-            }
-        }
-    }
 
-    /*
     IEnumerator PracticeGoNoGoGame()
     {
 
@@ -136,61 +94,7 @@ public class DecisionMakingTest_tuto : MonoBehaviour
         yield break;
 
     }
-    */
 
-    IEnumerator PracticeGoNoGoGame()
-    {
-
-        PracticeQuestionDisplay.text = '+'.ToString();
-        yield return new WaitForSeconds(1.5f); // 1.5초 동안 처음 준비 화면 보여줌
-
-        foreach (char q in PracticeGoNoGoQuestion)
-        {
-            ProcessCounting++;
-            buttonPressed = false;
-            ///Go 에서는 누른 시간, 놓친 여부 측정
-            if (PracticeGoList.Contains(q.ToString()))//눌러야 할 때
-            {
-
-                PracticeQuestionDisplay.text = q.ToString();
-                yield return new WaitForSeconds(0.3f); // 0.3초 동안 문제 보여줌
-                PracticeQuestionDisplay.text = "";//문제 사라짐
-                startTime = Time.time;
-                yield return new WaitForSeconds(1.8f); // 1.8초 동안 입력 대기
-
-                if (buttonPressed == true)
-                {
-                    Go_clicked++;
-                    elapsedTime = buttonTime - startTime;
-                    Sum_GoClickTime += elapsedTime;
-                }
-                else
-                {
-                    //Go인데 못누른 카운트 추가
-                    ++Go_missed;
-                }
-
-                yield return new WaitForSeconds(0.5f); // 0.5초 동안 휴식
-            }
-            else//누르지 말아야 할 때
-            {
-                PracticeQuestionDisplay.text = q.ToString();
-                yield return new WaitForSeconds(0.3f); // 0.3초 동안 문제 보여줌
-                PracticeQuestionDisplay.text = "";//문제 사라짐
-                yield return new WaitForSeconds(1.8f); // 1.8초 동안 입력 대기
-
-                if (buttonPressed == true)
-                {
-                    ++NoGo_clicked;
-                }
-
-                yield return new WaitForSeconds(0.5f); // 0.5초 동안 대기
-            }
-        }
-        PracticeQuestionDisplay.text = "";//대기 화면 보여줌
-        Counting();
-        yield break;
-    }
 
     /// <summary>
     /// 버튼이 눌리면 Go, NoGo 2가지로 나눠서 해야 함. 
@@ -203,16 +107,7 @@ public class DecisionMakingTest_tuto : MonoBehaviour
         SceneManager.LoadScene("Main_menu", LoadSceneMode.Single);
     }
 
-    private void ShowScore()
-    {
-        Avg_GoClickTime = Sum_GoClickTime / Go_clicked;
-        PracticeAvgResult = Avg_GoClickTime.ToString("F3");
 
-        DMT_Go_missed.text = "놓친 횟수: " + Go_missed + "번";
-        DMT_NoGo_clicked.text = "오입력 횟수 : " + NoGo_clicked + "번";
-        DMT_AvgResult.text = "평균 입력시간 : " + PracticeAvgResult + "초";
-        DMT_ProcessCounting.text = "진행도 : " + ProcessCounting + "/3회";
-    }
 
 
 }
